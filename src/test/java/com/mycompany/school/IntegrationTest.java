8857478beee8f3d8286fe6100a0ac9b9728d8977
Bhupender.Kumar <rajus92312@gmail.com>
2022-12-21 15:50:32 +0530

    Initial version of School generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/mycompany/school/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.mycompany.school;
+
+import com.mycompany.school.SchoolApp;
+import com.mycompany.school.config.AsyncSyncConfiguration;
+import com.mycompany.school.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { SchoolApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
